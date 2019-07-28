a27a99b3c0dd69cad82a1488f139bbcd789d6a54
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-07-28 01:54:27 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_012355) do
+ActiveRecord::Schema.define(version: 2019_07_28_012845) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
