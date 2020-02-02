83536afffd1ac41d84049e86c3d2840ef8df40c7
nicholasykng <nicholasykng@gmail.com>
2020-02-02 15:52:32 -0800

    Done.

---
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "posts/edit", :type => :view do
+  before(:each) do
+    @post = assign(:post, Post.create!(
+      :title => "MyString"
+    ))
+  end
+
+  
+end
---
+++ spec/views/posts/new.html.erb_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "posts/new", :type => :view do
+  before(:each) do
+    assign(:post, Post.new(
+      :title => "MyString"
+    ))
+  end
+
+  
+end
