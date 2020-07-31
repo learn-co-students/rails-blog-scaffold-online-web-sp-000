ccc3a2ca1a06bf5ee35e7855249082f4c2a11b86
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-31 21:00:50 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+# rails generate scaffold Post title:string text:string created_at:datetime updated_at:datetime --no-test-framework
+
+
+ActiveRecord::Schema.define(version: 2020_07_31_205357) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
