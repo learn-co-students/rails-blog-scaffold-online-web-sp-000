09523e385c88ff1a4efd1872fcb26e87b7bdacd0
codyalvarez <jedi.alvarez805@gmail.com>
2020-11-18 21:58:33 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:address, :price, :description, :image_url)
+      params.require(:post).permit(:title, :address, :price, :description, :image_url)
     end
 end
